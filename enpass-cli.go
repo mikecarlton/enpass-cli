54291defa7ff867b16e61ce0200a6f7c906f9c77
Mike Carlton <mike@carltons.us>
2017-04-30 14:28:37 -0700

    add card and user searching

--- enpass-cli.go
+++ enpass-cli.go
@@ -1,35 +1,23 @@
 
-package main
-
 /*
-  go get -v -u github.com/mattn/go-sqlite3
-  go get -v -u github.com/xeodou/go-sqlcipher
-  <link openssl into go-sqlcipher>
-    openssl -> /usr/local//Cellar/openssl/1.0.2i/include/openssl
+  Access Enpass from CLI
 
-  cd .../go-sqlite3
-  go install -v -ldflags '-L /usr/local/opt/sqlite/lib' -gcflags '-I /usr/local/opt/sqlite/include' -tags 'libsqlite3 darwin'
+  Copyright 2017 Mike Carlton
+  mike@carltons.us
 
-  cd .../go-sqlcipher
-  go install -v -ldflags '-L /usr/local/opt/sqlite/lib' -gcflags '-I /usr/local/opt/sqlite/include' -tags 'libsqlite3 darwin'
-
-  go build -v -tags 'libsqlite3 darwin'
-
-  # by hand:
-  sqlcipher <file>
-  PRAGMA cipher_default_kdf_iter = 24000;
-  PRAGMA kdf_iter = 24000;
-  PRAGMA key = '<password>';
-
-  # reference:
-  # https://github.com/steffen9000/enpass-decryptor/blob/master/Enpassant.py
+  Released under terms of the MIT License:
+    http://www.opensource.org/licenses/mit-license.php
 */
+
+package main
 
 import (
     "os"
-//    "log"
-//    "flag"
+    "runtime"
+    "syscall"
+    "flag"
     "fmt"
+    "strings"
     "encoding/json"
     "crypto/cipher"
     "crypto/aes"
@@ -50,7 +38,7 @@ func check(err error) {
 func generateKey(hash, salt []byte) (key []byte) {
     key = pbkdf2.Key(hash, salt, 2, 32, sha256.New)
 
-    return key
+    return
 }
 
 func getCryptoParams(db *sql.DB) (iv, key []byte) {
@@ -65,21 +53,20 @@ func getCryptoParams(db *sql.DB) (iv, key []byte) {
     salt := info[32:48]
     key = generateKey(hash, salt)
 
-    return iv, key
+    return
 }
 
 func openDb(db_file string) (db *sql.DB) {
+    if verbose {
+        fmt.Fprintf(os.Stderr, "Reading database file '%s'\n", db_file)
+    }
     db, err := sql.Open("sqlite3", db_file)
     check(err)
 
-    /*
-    t := terminal.NewTerminal(os.Stdin, "> ")
-    password, err := t.ReadPassword("Password: ")
-    */
     password, ok := os.LookupEnv("ENPASS_PASSWORD")
     if !ok {
-        fmt.Print("Password: ")
-        password_bytes, err := terminal.ReadPassword(0)
+        fmt.Fprint(os.Stderr, "Password: ")
+        password_bytes, err := terminal.ReadPassword(syscall.Stdin)
         check(err)
         password = string(password_bytes)
         fmt.Println()
@@ -97,7 +84,7 @@ func openDb(db_file string) (db *sql.DB) {
     _, err = db.Exec("PRAGMA kdf_iter = 24000;")
     check(err)
 
-    return db
+    return
 }
 
 func decrypt(data, iv, key []byte) (card_json []byte) {
@@ -114,7 +101,7 @@ func decrypt(data, iv, key []byte) (card_json []byte) {
     pad_bytes := int(data[len(data)-1])
     card_json = data[:len(data)-pad_bytes]
 
-    return card_json
+    return
 }
 
 type History struct {
@@ -143,10 +130,85 @@ type Card struct {
     Uuid string
 }
 
-func getCards(db *sql.DB, iv, key []byte) {
+// return field(s) of given type
+func (card Card) fieldsByType(typeName string) (fields []Field) {
+    typeName = strings.ToLower(typeName)
+    for _, f := range card.Fields {
+        if strings.ToLower(f.Type) == typeName {
+            fields = append(fields, f)
+        }
+    }
+
+    return
+}
+
+// return value of first non-empty field of given type
+func (card Card) firstByType(typeName string) (value string, err error) {
+    fields := card.fieldsByType(typeName)
+    if len(fields) > 0 && fields[0].Value != "" {
+        value = fields[0].Value
+    } else {
+        err = fmt.Errorf("Card has no fields of type '%s'", typeName)
+    }
+
+    return
+}
+
+func (card Card) getUser() (name string, err error) {
+    name, err = card.firstByType("username")
+    if err != nil {
+        name, err = card.firstByType("email")
+    }
+
+    if err != nil {
+        err = fmt.Errorf("Card has no username or email")
+    }
+
+    return
+}
+
+// return field(s) with type of given name
+func (card Card) fieldsMatchByType(name, value string) (fields []Field) {
+    name = strings.ToLower(name)
+    value = strings.ToLower(value)
+    for _, f := range card.Fields {
+        if strings.ToLower(f.Type) == name &&
+           strings.Contains(strings.ToLower(f.Value), value) {
+            fields = append(fields, f)
+        }
+    }
+
+    return
+}
+
+func (card Card) display(one_line, show_password bool) {
+    if one_line {
+        user, _ := card.getUser()
+        url, _ := card.firstByType("url")
+        password := ""
+        if show_password {
+            password, _ = card.firstByType("password")
+        }
+        fmt.Printf("%s: %s  %s  %s\n", card.Name, user, url, password)
+    } else {
+        fmt.Println(card.Name)
+
+        for _, f := range(card.Fields) {
+            value := f.Value
+            if f.Sensitive != 0 {
+                value = "*****"
+            }
+
+            fmt.Printf("\t%s (%s): %s\n", f.Label, f.Type, value)
+        }
+        fmt.Println("\tNote: ", card.Note)
+    }
+}
+
+func getCards(db *sql.DB, iv, key []byte) (cards []Card) {
     rows, err := db.Query("SELECT title, subtitle, data " +
                            "FROM Cards " +
-                           "WHERE deleted = 0 AND trashed = 0 LIMIT 10")
+                           "WHERE deleted = 0 AND trashed = 0")
     check(err)
     defer rows.Close()
 
@@ -171,33 +233,85 @@ func getCards(db *sql.DB, iv, key []byte) {
         }
         check(err)
 
-        fmt.Println(card.Name)
-        for _, f := range(card.Fields) {
-            value := f.Value
-            if f.Sensitive != 0 {
-                value = "*****"
-            }
-
-            fmt.Printf("\t%s (%s): %s\n", f.Label, f.Type, value)
+        if title != card.Name {
+            fmt.Println("Warning: card title '%s' != name '%s'",
+                        title, card.Name)
         }
-        fmt.Println("\tNote: ", card.Note)
+
+        cards = append(cards, card)
     }
 
     err = rows.Err()
     check(err)
     rows.Close()
+
+    return
 }
 
+var verbose = false
+
 func main() {
-    if len(os.Args) <= 1 {
+    flag.Usage = func() {
+        fmt.Fprintf(os.Stderr,
+                      "Usage: %s [options] [-a | NAME [USERNAME | EMAIL]]\n",
+                    os.Args[0])
+        flag.PrintDefaults()
+    }
+
+    var db_file string
+    var match_all, one_line, show_password bool
+
+    flag.StringVar(&db_file, "file", "", "enpass db file")
+    flag.BoolVar(&verbose, "v", false, "set verbose mode")
+    flag.BoolVar(&match_all, "a", false, "match all records")
+    flag.BoolVar(&one_line, "o", false, "show records on one line")
+    flag.BoolVar(&show_password, "p", false, "show passwords")
+    flag.Parse()
+
+    if db_file == "" {
+        db_file = os.Getenv("ENPASS_DB_FILE")
+    }
+
+    if db_file == "" {
+        if runtime.GOOS == "darwin" {
+            home, ok := os.LookupEnv("HOME")
+            if ok {
+                db_file = home + "/Documents/Enpass/walletx.db"
+            }
+        }
+    }
+
+    if db_file == "" {
         fmt.Println("filename required")
         return
     }
 
-    db := openDb(os.Args[1])
+    if match_all && flag.NArg() != 0 ||
+       !match_all && (flag.NArg() < 1 || flag.NArg() > 2) {
+        flag.Usage()
+        return
+    }
+
+    name := strings.ToLower(flag.Arg(0))
+    match_user := flag.NArg() == 2
+    user := strings.ToLower(flag.Arg(1))
+
+    db := openDb(db_file)
     defer db.Close()
 
     iv, key := getCryptoParams(db)
 
-    getCards(db, iv, key)
+    cards := getCards(db, iv, key)
+    for _, card := range cards {
+        match := match_all || strings.Contains(strings.ToLower(card.Name), name)
+        if match && match_user {
+            cardUser, err := card.getUser()
+            match = err == nil &&
+                        strings.Contains(strings.ToLower(cardUser), user)
+        }
+
+        if match {
+            card.display(one_line, show_password)
+        }
+    }
 }
